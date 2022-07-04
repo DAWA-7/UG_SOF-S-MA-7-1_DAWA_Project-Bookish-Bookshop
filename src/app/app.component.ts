b06ee058ef9ac3e7b4b37f93eb46c7aca2051bf4
nefi.reyest@ug.edu.ec <WxYhvDn4XYQ5fsL>
2022-07-04 12:12:21 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SOF-S-MA-7-1_DAWA-Project_Bookish-Bookshop';
+}
